74da8b27ae7b3e1c91487c4c4d911ad4c06c99c4
daisixuan <282280272@qq.com>
2021-03-28 16:41:02 +0800

    first commit

--- hook_artmethod_register.js
+++ hook_artmethod_register.js
@@ -1,10 +1,3 @@
-/*
-1. 将libext.so 和libext64.so 放到 /data/app 目录下, chmod 777 , 并setenforce 0
-2. frida -U -f 包名 -l hook_artmethod_register.js --no-pause
-包含yang神的hook RegisterNatives 可以比较是否相同 参考 yang神的hook artmethod.js 来打印 ArtMethod* 得到函数的方法和签名 
-hook ArtMethod::RegisterNative 可以得到更多的信息,动态注册无法逃离这个函数,防止某些app自实现动态注册,不走jni的registerNatives 
-*/
-
 function hook_ArtMethodRegister() {
     var symbols = Module.enumerateSymbolsSync("libart.so");
 
@@ -44,11 +37,6 @@ function hook_ArtMethodRegister() {
             Interceptor.attach(ArtMethodRegisterNative, {
                 onEnter: function (args) {
                     try {
-                        // console.log(this.context.x0);
-                        // console.log(this.context.x1);
-                        // console.log(this.context.x2);
-                        // console.log(this.context.x3);
-                        // console.log(args[0],args[1],args[2],args[3])
                         var result = Memory.alloc(0x100);
                         var fnPtr_ptr = args[1];
                         var find_module = Process.findModuleByAddress(fnPtr_ptr);
@@ -90,7 +78,6 @@ function hook_RegisterNatives() {
                 var env = args[0];
                 var java_class = args[1];
                 var class_name = Java.vm.tryGetEnv().getClassName(java_class);
-                //console.log(class_name);
 
                 var methods_ptr = ptr(args[2]);
 
@@ -111,7 +98,6 @@ function hook_RegisterNatives() {
     }
 }
 
-//setImmediate(hook_native);
 setImmediate(hook_ArtMethodRegister)
 setImmediate(hook_RegisterNatives);
 
